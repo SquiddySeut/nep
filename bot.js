4eeb7e073b2f4185a95fe5a0e4d9be26e81cbeaf
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2018-07-19 20:07:49 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,7 @@ client.on('message', message => {
         message.channel.send("nep nep");
     }
     if (message.content==="nep nep"){
+        nep=0;
         while(nep<10){
             message.channel.send("nep nep nep nep nep nep nep nep nep");
             nep++;

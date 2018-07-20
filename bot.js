9f4320ce40a779de4fcef3bf6db110055e9a4007
SquiddySeut <40527468+SquiddySeut@users.noreply.github.com>
2018-07-19 20:31:32 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+var nepcount;
+var n;
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -15,9 +16,39 @@ client.on('message', message => {
     if (message.content==="Nep"){
         message.channel.send("nep nep");
     }
-    if (message.content==="nep nep"){
-        nep=0;
-        while(nep<30){
+    if (message.content==="nepcount){
+        message.reply("What's the nepcount?");
+        n=1;
+        nepcount=10;
+    }
+    if (message.content==="1"&&n>=1){
+        message.reply("set to one");
+        n=2;
+        nepcount=10;
+    }
+     if (message.content==="2"&&n>=1){
+        message.reply("set to two");
+        n=2;
+        nepcount=20;
+    }
+     if (message.content==="3"&&n>=1){
+        message.reply("set to three");
+        n=2;
+        nepcount=30;
+    }
+     if (message.content==="4"&&n>=1){
+        message.reply("set to four");
+        n=2;
+        nepcount=40;
+    }
+     if (message.content==="5"&&n>=1){
+        message.reply("set to five");
+        n=2;
+        nepcount=50;
+    }
+    if (message.content==="nep nep"&&n=2){
+        nep=2;
+        while(nep<nepcount){
             message.channel.send("nep nep nep nep nep nep nep nep nep");
             nep++;
         }
